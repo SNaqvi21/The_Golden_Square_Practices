aa3b5cc765b2b036f8d3f3fc925384fe7f95d0ef
syed naqvi <snaqvi21@hotmail.com>
2025-10-14 20:46:44 +0100

    debugging_most_common_letter.py

--- lib/most_common_letter_debuging_challenge.py
+++ lib/most_common_letter_debuging_challenge.py
@@ -3,10 +3,14 @@ def get_most_common_letter(text):
     for char in text:
         counter[char] = counter.get(char, 0) + 1
         print(char)
-    letter = sorted(counter.items(), key=lambda item: item[1])[0][1]
+    letter = sorted(counter.items(), key=lambda item: item[1], reverse=True)[1][0] # this line was where the bug was
     print(letter)
     return letter
-
+    # THE bug HAS BEEN FIXED. It was on line 6. 
+    # The key=lambda method applied on the letter list on line 6, was indexing the least common letter in the text "(item[1])[0][1]""
+    # as the list returned was in asccending order, we had to reverse it and then apply
+    # the correct index for "e" which we found by printing the letter list
+    # in the Debug console to pin point the index for the expected "e" letter. 
 
 print(f"""
 Running:  get_most_common_letter("the roof, the roof, the roof is on fire!"))
